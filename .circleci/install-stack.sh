28b987649c31cce6c9791f5084994125f354eee6
takimoto <takimoto@shiftall.net>
2018-04-23 17:49:38 +0900

    Fix install-stack.sh

--- .circleci/install-stack.sh
+++ .circleci/install-stack.sh
@@ -6,7 +6,7 @@ then
 else
     echo "Start Download and install stack."
     mkdir -p ~/.local/bin
-    curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --strip-components=1 -C ~/.local/bin '*/stack'
+    curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 fi
 
 export PATH=$HOME/.local/bin:$PATH
